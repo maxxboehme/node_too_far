9288fbe640da7fc225bc34282751af35cd6166da
Maxx Boehme <maxxboehme@gmail.com>
2014-10-13 07:21:09 -0700

    Combined into one function

--- source/Node.cpp
+++ source/Node.cpp
@@ -86,3 +86,67 @@ void node_too_far_solve(std::istream& in, std::ostream& out){
       }
    }
 }
+
+
+// Was surprisingly no faster then just using the inline functions.
+void all_in_on_node_too_far_solve(std::istream& in, std::ostream& out){
+   // turn off synchronization with C I/O
+   std::ios_base::sync_with_stdio(false);
+   
+   int numberOfPairs = 0;
+   int caseNumber = 0;
+   
+   while(in >> numberOfPairs && numberOfPairs){
+      std::array<std::vector<int>, 30> graph;
+      std::unordered_map<int, int> mappingToIndex;
+
+      int node1;
+      int node2;
+
+      int index = 0;
+      while(--numberOfPairs >= 0 && in >> node1 >> node2){
+         if(mappingToIndex.find(node1) == mappingToIndex.end()) {
+            mappingToIndex[node1] = index++;
+         }
+         if(mappingToIndex.find(node2) == mappingToIndex.end()) {
+            mappingToIndex[node2] = index++;
+         }
+         graph[mappingToIndex[node1]].push_back(mappingToIndex[node2]);
+         graph[mappingToIndex[node2]].push_back(mappingToIndex[node1]);
+      }      
+
+
+
+      int graphSize = mappingToIndex.size();
+      int start = 0;
+      int ttl = 0;
+      while(in >> start >> ttl && (start || ttl)){
+         int result = 0;
+         if(mappingToIndex.find(start) != mappingToIndex.end()){
+            int cost[30];
+            memset (cost, -1, sizeof (cost));
+
+            std::queue<int> fringe;
+            fringe.push(mappingToIndex[start]);
+
+            cost[mappingToIndex[start]] = 0;
+            int reachable = 0;
+            while(!fringe.empty()){
+               int node = fringe.front();
+               fringe.pop();
+               ++reachable;
+               if((ttl - cost[node]) > 0){
+                  for(int child: graph[node]){
+                     if(cost[child] == -1){
+                        cost[child] = cost[node] + 1;
+                        fringe.push(child);
+                     }
+                  }
+               }
+             }
+             result = reachable;
+          }
+          out << "Case " << ++caseNumber << ": " << (graphSize - result) << " nodes not reachable from node " << start << " with TTL = " << ttl << "." << std::endl;
+      }
+   }
+}
